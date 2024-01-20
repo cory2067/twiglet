04a3a65b1e6dc5778eccce65c3d5a7b69159df08
Cyn Liu <cynthialiustar@gmail.com>
2024-01-20 14:56:20 -0800

    untested syntax error

--- base_objects.py
+++ base_objects.py
@@ -12,14 +12,14 @@ beaker_debuff = Modifier(**beaker_debuff)
 
 beaker_attributes = {
     'name': 'Beaker',
-    'object_description': 'A glass beaker with a small amount of unknown, corrosive and toxic fluid'
-    'attack_description': 'Splash the fluid onto your opponent'
-    'durability': 1
-    'accuracy': 50
-    'speed': 4
-    'strength': 10
+    'object_description': 'A glass beaker with a small amount of unknown, corrosive and toxic fluid',
+    'attack_description': 'Splash the fluid onto your opponent',
+    'durability': 1,
+    'accuracy': 50,
+    'speed': 4,
+    'strength': 10,
     'debuff': beaker_debuff,
-    'buff': None
+    'buff': None,
 }
 beaker = GameObject(**beaker_attributes)
 
