a3c4d53149a89f69bdb95629dcc80037373cc64a
Cory Lynch <cjl2625@gmail.com>
2024-01-20 19:32:24 -0800

    Add some more objects

--- base_objects.py
+++ base_objects.py
@@ -1,5 +1,7 @@
 from game_object import GameObject, Modifier
 
+OBJECTS = []
+
 beaker_debuff = Modifier(
     stat_affected="accuracy",
     value=20,
@@ -14,11 +16,13 @@ beaker = GameObject(
     durability=1,
     accuracy=50,
     speed=4,
-    strength=10,
+    strength=3,
     debuff=beaker_debuff,
     buff=None,
     file="beaker.png",
 )
+
+OBJECTS.append(beaker)
 
 # ====================================================
 
@@ -42,6 +46,8 @@ beer = GameObject(
     file="beer.png",
 )
 
+OBJECTS.append(beer)
+
 # ====================================================
 
 bone = GameObject(
@@ -57,6 +63,8 @@ bone = GameObject(
     file="bone.png",
 )
 
+OBJECTS.append(bone)
+
 # ====================================================
 
 drill = GameObject(
@@ -71,6 +79,8 @@ drill = GameObject(
     buff=None,
     file="drill.png",
 )
+
+OBJECTS.append(drill)
 
 # ====================================================
 
@@ -94,6 +104,8 @@ microphone = GameObject(
     file="microphone.png",
 )
 
+OBJECTS.append(microphone)
+
 # ====================================================
 
 wallet_debuff = Modifier(
@@ -116,11 +128,134 @@ wallet = GameObject(
     file="wallet.png",
 )
 
-OBJECTS = [
-    beaker,
-    beer,
-    bone,
-    drill,
-    microphone,
-    wallet,
-]
+OBJECTS.append(wallet)
+
+# ====================================================
+
+pickaxe = GameObject(
+    name="Pickaxe",
+    object_description="A heavy iron pickaxe",
+    attack_description="Swing the pickaxe at your opponent",
+    durability=3,
+    accuracy=85,
+    speed=2,
+    strength=9,
+    debuff=None,
+    buff=None,
+    file="pickaxe.png",
+)
+
+OBJECTS.append(pickaxe)
+
+# ====================================================
+
+toothbrush_debuff = Modifier(
+    stat_affected="accuracy",
+    value=5,
+    duration=2,
+    reason="The opponent is mildly distracted from worrying about their dental hygiene",
+)
+
+toothbrush = GameObject(
+    name="Toothbrush",
+    object_description="A cheap disposable toothbrush",
+    attack_description="Start violently brushing your opponent's teeth",
+    durability=2,
+    accuracy=70,
+    speed=3,
+    strength=1,
+    debuff=toothbrush_debuff,
+    buff=None,
+    file="toothbrush.png",
+)
+
+OBJECTS.append(toothbrush)
+
+# ====================================================
+
+wire_clippers_debuff = Modifier(
+    stat_affected="strength",
+    value=0.2,
+    duration=5,
+    reason="The opponent's weapon is breaks, making it much weaker",
+)
+
+wire_clippers = GameObject(
+    name="Wire Clippers",
+    object_description="A pair of wire clippers",
+    attack_description="Attempt to snip your opponent's weapon in half",
+    durability=1,
+    accuracy=50,
+    speed=2,
+    strength=0,
+    debuff=None,
+    buff=None,
+    file="wire_clippers.png",
+)
+
+OBJECTS.append(wire_clippers)
+
+# ====================================================
+
+coffee_pot_buff = Modifier(
+    stat_affected="speed",
+    value=1,
+    duration=3,
+    reason="Take a quick sip of coffee before attacking",
+)
+
+coffee_pot = GameObject(
+    name="Coffee Pot",
+    object_description="A coffee pot filled with hot coffee",
+    attack_description="Spill scalding hot coffee on your opponent",
+    durability=1,
+    accuracy=90,
+    speed=2,
+    strength=6,
+    debuff=None,
+    buff=coffee_pot_buff,
+    file="coffee_pot.png",
+)
+
+OBJECTS.append(coffee_pot)
+
+# ====================================================
+
+computer = GameObject(
+    name="Computer tower",
+    object_description="A bulky computer tower from the 90s",
+    attack_description="Lob the computer at your opponent",
+    durability=1,
+    accuracy=60,
+    speed=1,
+    strength=4,
+    debuff=None,
+    buff=None,
+    file="computer.png",
+)
+
+OBJECTS.append(computer)
+
+# ====================================================
+
+pen_debuff = Modifier(
+    stat_affected="accuracy",
+    value=100,
+    duration=4,
+    reason="The opponent is persuaded by your brilliant essay and stops fighting",
+)
+
+pen = GameObject(
+    name="Pen",
+    object_description="The pen is mightier than the sword...?",
+    attack_description="Write the opponent an essay about the futility of violence",
+    durability=1,
+    accuracy=5,
+    speed=1,
+    strength=0,
+    debuff=None,
+    buff=None,
+    file="pen.png",
+)
+
+OBJECTS.append(pen)
--- game_object.py
+++ game_object.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
-
+from typing import Literal
 
 class Modifier(BaseModel):
-    stat_affected: str
-    value: int
+    stat_affected: Literal["accuracy", "speed", "strength"]
+    value: float
     duration: int
     reason: str
 
